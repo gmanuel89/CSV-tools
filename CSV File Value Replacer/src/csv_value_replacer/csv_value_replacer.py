779eaf65eb0e8d43ee12d19a54c7a6163b409cbf
gmanuel89 <manuel.galli@revvity.com>
2026-09-08 10:00:31 +0200

    improved logging of changes

--- CSV File Value Replacer/src/csv_value_replacer/csv_value_replacer.py
+++ CSV File Value Replacer/src/csv_value_replacer/csv_value_replacer.py
@@ -40,49 +40,68 @@ def create_replacing_map(input_dataframe: pandas.DataFrame) -> list[dict]:
 
 ## Replace the csv values according to a list of dictionaries with 'old' and 'new' values
 def replace_csv_values(input_dataframe: pandas.DataFrame, mapping_dictionary_array: list[dict], columns_to_print_in_log=[]) -> pandas.DataFrame:
-    ## Run if there is a map (otherwise return the input file with no modifications)
-    if len(mapping_dictionary_array) == 0: return input_dataframe
-    ## Scroll the replacing map items...
+    # Run if there is a map (otherwise return the input file with no modifications)
+    if len(mapping_dictionary_array) == 0:
+        app_logger.warning('No replacement will occur!')
+        return input_dataframe
+    app_logger.debug('Replacing values in CSV file...')
+    # Scroll the replacing map items...
     for maprepl in mapping_dictionary_array:
-        ## If there is no column(s) specified, go for the all-cell replacement
-        if not maprepl.get('columns'):
-            # For each row...
-            for index, row in input_dataframe.iterrows():
+        # Get replacement information
+        columns_for_replacements = maprepl.get('columns', [])
+        old_value = maprepl.get('old')
+        new_value = maprepl.get('new')
+        # For each row of the DataFrame...
+        for index, row in input_dataframe.iterrows():
+            # Store values for logging
+            row_modified = False
+            row_old_value = row.copy()
+            row_new_value = row.copy()
+            # If there is no column(s) specified, go for the all-cell replacement
+            if not columns_for_replacements:
                 # For each column...
                 for col in input_dataframe.columns:
                     # Replace the cell values
-                    if row[col] == str(maprepl.get('old')):
-                        app_logger.info(
-                        f"Row {row.name}, Column '{col}': "
-                        f"'{row[col]}' → '{str(maprepl.get('new'))}'"
-                        )
-                        if columns_to_print_in_log:
-                            row_to_print = {}
-                            for c in columns_to_print_in_log:
-                                if c in row.to_dict().keys():
-                                    row_to_print[c] = row.to_dict().get(c)
-                        else:
-                            row_to_print = row.to_dict()
-                        app_logger.debug(f'Updating row:\n{row_to_print}')
-                        input_dataframe.at[row.name, col] = str(maprepl.get('new'))
-        else:
-            ## If there are columns specified
-            # Determine the indices of the columns (compare mapping with the header)
-            columns = maprepl.get('columns',[])
-            # For each row...
-            for index, row in input_dataframe.iterrows():
+                    if row[col] == old_value:
+                        app_logger.debug(f"Row {row.name}, Column '{col}': '{row[col]}' → '{new_value}'")
+                        # Update the original DataFrame
+                        input_dataframe.at[row.name, col] = new_value
+                        # Store the row change for logging
+                        row_new_value[col] = new_value
+                        row_modified = True
+            # If there are columns specified
+            else:
                 # For each column...
                 for col in input_dataframe.columns:
-                    if col in columns:
+                    if col in columns_for_replacements:
                         # Replace the cell values
-                        if row[col] == str(maprepl.get('old')):
-                            app_logger.info(
-                            f"Row {row.name}, Column '{col}': "
-                            f"'{row[col]}' → '{str(maprepl.get('new'))}'"
-                            )
-                            input_dataframe.at[row.name, col] = str(maprepl.get('new'))
+                        if row[col] == old_value:
+                            app_logger.debug(f"Row {row.name}, Column '{col}': '{row[col]}' → '{new_value}'")
+                            # Update the original DataFrame
+                            input_dataframe.at[row.name, col] = new_value
+                            # Store the row change for logging
+                            row_new_value[col] = new_value
+                            row_modified = True
+            # Log only in case of modifications
+            if row_modified:
+                log_row_value_replacement(row_old_value, columns_to_print_in_log, 'Row updated (old values):\n')
+                log_row_value_replacement(row_new_value, columns_to_print_in_log, 'Row updated (new values):\n')
     # Return
     return input_dataframe
+
+## Log the row value with a specific set of columns
+def log_row_value_replacement(row: pandas.Series, columns_to_print_in_log: list[str], log_phrase='Updating row:\n') -> None:
+    """Log the row value with a specific set of columns"""
+    # Compare the columns in the DataFrame with the ones provided
+    row_columns = set(row.index)
+    provided_columns_to_print_in_log = set(columns_to_print_in_log)
+    final_columns_to_print_in_log = row_columns.intersection(provided_columns_to_print_in_log)
+    if final_columns_to_print_in_log:
+        row_to_print = {col: row[col] for col in final_columns_to_print_in_log}
+        app_logger.debug(f'{log_phrase}{row_to_print}')
+    else:
+        row_to_print = row.to_dict()
+        app_logger.debug(f'{log_phrase}{row_to_print}')
 
 ## Replace the csv values according to a list of dictionaries with 'old' and 'new' values
 def replace_csv_values2(input_dataframe: pandas.DataFrame, mapping_dictionary_array: list[dict]) -> pandas.DataFrame:
